130145a6d81eca99d580e5e7e2f5b949dfd5d834
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-07-23 19:57:44 +1000

    Paginate data initialization

--- htdocs/maestrano/scripts/init_script.php
+++ htdocs/maestrano/scripts/init_script.php
@@ -4,15 +4,15 @@
 // Scripts are run only once on application initialize
 
 // Run init scripts
-$init_script_file = MAESTRANO_ROOT . '/var/_init_scripts';
-$init_script_content = openAndReadFile($init_script_file);
-$script_dirs = MAESTRANO_ROOT . '/init/scripts';
+$init_script_file = 'maestrano/var/_init_scripts';
+$init_script_content = file_get_contents($init_script_file);
+$script_dirs = 'maestrano/scripts/scripts';
 $script_files = array_diff(scandir($script_dirs), array('..', '.'));
 
 // Iterate over already loaded scripts
 foreach ($script_files as $script_file) {
   $contained = strpos($init_script_content, $script_file);
-  if($contained !== 0) {
+  if($contained === false) {
     // Run script file
     require_once($script_dirs . "/" . $script_file);
     file_put_contents($init_script_file, $script_file . "\n", FILE_APPEND);
